894d64a1524bf559e0d75d504d1c03d82b0c1786
Fernando Noguera <fernandonogueram95@gmail.com>
2025-10-20 18:07:37 -0600

    menu de opciones implementado

--- VendingMachine/VendingMachine/Form1.cs
+++ VendingMachine/VendingMachine/Form1.cs
@@ -23,19 +23,61 @@ namespace VendingMachine
             bproducto1.Image = cheetos;
             bproducto1.BackgroundImageLayout = ImageLayout.Stretch;
 
-            Image cahoy = Image.FromFile(@"assets\logos\chips ahoy.png");
-            bproducto2.Image = cahoy;
+            Image chipAhoy = Image.FromFile(@"assets\logos\chips ahoy.png");
+            bproducto2.Image = chipAhoy;
             bproducto2.BackgroundImageLayout = ImageLayout.Stretch;
 
-            Image ccola = Image.FromFile(@"assets\logos\coca cola.png");
-            bproducto3.Image = ccola;
+            Image cocaCola = Image.FromFile(@"assets\logos\coca cola.png");
+            bproducto3.Image = cocaCola;
             bproducto3.BackgroundImageLayout = ImageLayout.Stretch;
 
             Image doritos = Image.FromFile(@"assets\logos\doritos.png");
             bproducto4.Image = doritos;
             bproducto4.BackgroundImageLayout = ImageLayout.Stretch;
 
+            Image gatorade = Image.FromFile(@"assets\logos\gatorade.png");
+            bproducto5.Image = gatorade;
+            bproducto5.BackgroundImageLayout = ImageLayout.Stretch;
 
+            Image kitKat = Image.FromFile(@"assets\logos\kit kat.png");
+            bproducto6.Image = kitKat;
+            bproducto6.BackgroundImageLayout = ImageLayout.Stretch;
+
+            Image monster = Image.FromFile(@"assets\logos\monster.png");
+            bproducto7.Image = monster;
+            bproducto7.BackgroundImageLayout = ImageLayout.Stretch;
+
+            Image oreo = Image.FromFile(@"assets\logos\oreo.png");
+            bproducto8.Image = oreo;
+            bproducto8.BackgroundImageLayout = ImageLayout.Stretch;
+
+            Image pepsi = Image.FromFile(@"assets\logos\pepsi.png");
+            bproducto9.Image = pepsi;
+            bproducto9.BackgroundImageLayout = ImageLayout.Stretch;
+
+            Image redBull = Image.FromFile(@"assets\logos\red bull.png");
+            bproducto10.Image = redBull;
+            bproducto10.BackgroundImageLayout = ImageLayout.Stretch;
+
+            Image skittles = Image.FromFile(@"assets\logos\skittles.png");
+            bproducto11.Image = skittles;
+            bproducto11.BackgroundImageLayout = ImageLayout.Stretch;
+
+            Image snickers = Image.FromFile(@"assets\logos\snickers.png");
+            bproducto12.Image = snickers;
+            bproducto12.BackgroundImageLayout = ImageLayout.Stretch;
+
+            Image sprite = Image.FromFile(@"assets\logos\sprite.png");
+            bproducto13.Image = sprite;
+            bproducto13.BackgroundImageLayout = ImageLayout.Stretch;
+
+            Image drPepper = Image.FromFile(@"assets\logos\dr pepper.png");
+            bproducto14.Image = drPepper;
+            bproducto14.BackgroundImageLayout = ImageLayout.Stretch;
+
+            Image fanta = Image.FromFile(@"assets\logos\fanta.png");
+            bproducto15.Image = fanta;
+            bproducto15.BackgroundImageLayout = ImageLayout.Stretch;
         }
     }
 }
